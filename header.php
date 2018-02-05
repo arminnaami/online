73f35710cba38745eafe099e6aa9b53147404480
Deep Bratt Mondal <deepbratt@gmail.com>
2018-02-05 19:12:47 +0530

    das
    adsd

--- header.php
+++ header.php
@@ -1,79 +1,79 @@
-      <nav class="navbar navbar-expand-lg mai-sub-header" style="border-radius: 3px;box-shadow: 0 1px 2px 0 rgba(0,0,0,0.25);">
-        <div class="container">
-          <!-- Mega Menu structure-->
-          <nav class="navbar navbar-expand-md ">
-            <button type="button" data-toggle="collapse" data-target="#mai-navbar-collapse" aria-controls="#mai-navbar-collapse" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler hidden-md-up collapsed" style="float:right;border:1px solid white;"> 
-              <div class="icon-bar">
-                <span>
-                </span>
-                <span>
-                </span>
-                <span>
-                </span>
-              </div>
-            </button>
+<nav class="navbar navbar-expand-lg mai-sub-header" style="border-radius: 3px;box-shadow: 0 1px 2px 0 rgba(0,0,0,0.25);">
+<div class="container">
+  <!-- Mega Menu structure-->
+  <nav class="navbar navbar-expand-md ">
+	<button type="button" data-toggle="collapse" data-target="#mai-navbar-collapse" aria-controls="#mai-navbar-collapse" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler hidden-md-up collapsed" style="float:right;border:1px solid white;"> 
+	  <div class="icon-bar">
+		<span>
+		</span>
+		<span>
+		</span>
+		<span>
+		</span>
+	  </div>
+	</button>
 
-            <div id="mai-navbar-collapse" class="navbar-collapse collapse mai-nav-tabs" style="margin-top:-40px;padding:10px;">
-              <ul class="nav navbar-nav" style="width:100%;">
-                <li class="nav-item parent open" >
-                  <a href="#" role="button" class="nav-link" style="background:none;height:70px;width:100%;">
-                    <img src="images/logodof.png" style="height:70px;">
-					<div class="clearfix"></div>
-                  </a>
+	<div id="mai-navbar-collapse" class="navbar-collapse collapse mai-nav-tabs" style="margin-top:-40px;padding:10px;">
+	  <ul class="nav navbar-nav" style="width:100%;">
+		<li class="nav-item parent open" >
+		  <a href="#" role="button" class="nav-link" style="background:none;height:70px;width:100%;">
+			<img src="images/logodof.png" style="height:70px;">
+			<div class="clearfix"></div>
+		  </a>
 
-				  <ul class="mai-nav-tabs-sub mai-sub-nav nav">
-					<li class="nav-item">
-                      <a href="index.php" class="nav-link">
-                        <span class="name">Home
-                        </span>
-                      </a>
-                    </li>
+		  <ul class="mai-nav-tabs-sub mai-sub-nav nav">
+			<li class="nav-item">
+			  <a href="index.php" class="nav-link">
+				<span class="name">Home
+				</span>
+			  </a>
+			</li>
 
-                    <li class="nav-item">
-                      <a href="hscode.php" class="nav-link active">
-                        <span class="name">HS Codes
-                        </span>
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a href="weight_metric.php" class="nav-link">
-                        <span class="name">Weight Mass
-                        </span>
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a href="length_dist_metric.php" class="nav-link">
-                        <span class="name">Length Distance
-                        </span>
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a href="volume_metric.php" class="nav-link">
-                        <span class="name">Capacity & Volume
-                        </span>
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a href="temp_convert.php" class="nav-link">
-                        <span class="name"> Temperature
-                        </span>
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-				
-				<li class="nav-item parent open" style="float:right;width:70%;background:white;border-radius: 3px;box-shadow: 0 1px 2px 0 rgba(0,0,0,0.25);">
-                  <!--<a href="#" role="button" class="nav-link" style="background:none;">
-                    <img src="images/images.png" style="height:70px;width:100%;" class="img-responsive">
-					<div class="clearfix"></div>
-                  </a>-->
-				  <h1 style="text-align:center;padding-top:15px;">Ad block</h1>
-				</li>
-              </ul>
-			  
-            </div>
-          </nav>
-          <!--Search input-->
+			<li class="nav-item">
+			  <a href="hscode.php" class="nav-link active">
+				<span class="name">HS Codes
+				</span>
+			  </a>
+			</li>
+			<li class="nav-item">
+			  <a href="weight_metric.php" class="nav-link">
+				<span class="name">Weight Mass
+				</span>
+			  </a>
+			</li>
+			<li class="nav-item">
+			  <a href="length_dist_metric.php" class="nav-link">
+				<span class="name">Length Distance
+				</span>
+			  </a>
+			</li>
+			<li class="nav-item">
+			  <a href="volume_metric.php" class="nav-link">
+				<span class="name">Capacity & Volume
+				</span>
+			  </a>
+			</li>
+			<li class="nav-item">
+			  <a href="temp_convert.php" class="nav-link">
+				<span class="name"> Temperature
+				</span>
+			  </a>
+			</li>
+		  </ul>
+		</li>
+		
+		<li class="nav-item parent open" style="float:right;width:70%;background:white;border-radius: 3px;box-shadow: 0 1px 2px 0 rgba(0,0,0,0.25);">
+		  <!--<a href="#" role="button" class="nav-link" style="background:none;">
+			<img src="images/images.png" style="height:70px;width:100%;" class="img-responsive">
+			<div class="clearfix"></div>
+		  </a>-->
+		  <h1 style="text-align:center;padding-top:15px;">Ad block</h1>
+		</li>
+	  </ul>
+	  
+	</div>
+  </nav>
+  <!--Search input-->
 
-        </div>
-      </nav>
+</div>
+</nav>
