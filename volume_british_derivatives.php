2583ff018110b6dbf128227f197f7146a49f8c21
Sujit Sarkar <sweezit92@gmail.com>
2018-02-07 14:02:33 +0530

    efe

--- volume_british_derivatives.php
+++ volume_british_derivatives.php
@@ -29,6 +29,11 @@
         border-radius: .25rem;
         transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
       }
+	  .form-control-xs::-webkit-inner-spin-button, 
+	  .form-control-xs::-webkit-outer-spin-button { 
+		  -webkit-appearance: none; 
+		  margin: 0; 
+		}
     </style>
   </head>
   <body>
@@ -51,6 +56,9 @@
                 </span>
               </div>
             </div>
+			<div class="panel panel-default" style="background:white;height:100px;padding:10px;background:#fe8458;border-radius: 3px;box-shadow: 0 1px 2px 0 rgba(0,0,0,0.25);margin-bottom:20px;">
+				<h1 style="text-align:center;color:white;font-weight:bold;padding:25px;">Ad Block</h1>
+			</div>
 			<div class="panel panel-default" style="background:white;border-radius: 3px;box-shadow: 0 1px 2px 0 rgba(0,0,0,0.25);">
               <div class="panel-heading panel-heading-divider" style="padding:14px 20px;margin:0px;">
                 <h4>British and U.S. derivatives of length units</h4>
@@ -62,13 +70,13 @@
                       <label>cubic mile (mi&sup3;)</label>
                     </div>
                     <div class="col-sm-3">
-                      <input type="text" class="boots_form-control form-control-xs">
+                      <input type="number" class="boots_form-control form-control-xs mile" onkeyup="from_mile();" onkeydown="javascript: return event.keyCode == 69 ? false : true">
                     </div>
                     <div class="col-sm-3">
                       <label>centum cubic feet (ccf)</label>
                     </div>
                     <div class="col-sm-3">
-                      <input type="text" class="boots_form-control form-control-xs">
+                      <input type="number" class="boots_form-control form-control-xs ccf" onkeyup="from_ccf();" onkeydown="javascript: return event.keyCode == 69 ? false : true">
                     </div>
                   </div>
                   <div class="form-group inline row">
@@ -76,13 +84,13 @@
                       <label>mil cubic feet (mcf)</label>
                     </div>
                     <div class="col-sm-3">
-                      <input type="text" class="boots_form-control form-control-xs">
+                      <input type="number" class="boots_form-control form-control-xs mcf" onkeyup="from_mcf();" onkeydown="javascript: return event.keyCode == 69 ? false : true">
                     </div>
                     <div class="col-sm-3">
                       <label>cubic foot (ft&sup3;)</label>
                     </div>
                     <div class="col-sm-3">
-                      <input type="text" class="boots_form-control form-control-xs">
+                      <input type="number" class="boots_form-control form-control-xs foot" onkeyup="from_foot();" onkeydown="javascript: return event.keyCode == 69 ? false : true">
                     </div>
                   </div>
                   <div class="form-group inline row">
@@ -90,18 +98,21 @@
                       <label>cubic yard (yd&sup3;)</label>
                     </div>
                     <div class="col-sm-3">
-                      <input type="text" class="boots_form-control form-control-xs">
+                      <input type="number" class="boots_form-control form-control-xs yard" onkeyup="from_yard();" onkeydown="javascript: return event.keyCode == 69 ? false : true">
                     </div>
                     <div class="col-sm-3">
                       <label>cubic inch (in&sup3;)</label>
                     </div>
                     <div class="col-sm-3">
-                      <input type="text" class="boots_form-control form-control-xs">
+                      <input type="number" class="boots_form-control form-control-xs inch" onkeyup="from_inch();" onkeydown="javascript: return event.keyCode == 69 ? false : true">
                     </div>
                   </div>
                 </form>
               </div>
             </div>
+			<div class="panel panel-default" style="background:white;height:100px;padding:10px;background:#fe8458;border-radius: 3px;box-shadow: 0 1px 2px 0 rgba(0,0,0,0.25);margin-bottom:20px;">
+				<h1 style="text-align:center;color:white;font-weight:bold;padding:25px;">Ad Block</h1>
+			</div>
 		 </div>
            <?php include("right_sidebar.php");?>
           <!--Date Picker-->
@@ -127,5 +138,136 @@
       }
                        );
     </script>
+	<script>
+	function from_mile()
+	{
+		var mile = $(".mile").val();
+
+		var ccf = mile * 1.472e+9;
+		$(".ccf").val(ccf);
+
+		var mcf = mile * 1.472e+11;
+		$(".mcf").val(mcf);
+
+		var foot = mile * 1.472e+11;
+		$(".foot").val(foot);
+
+		var yard = mile * 5.452e+9;
+		$(".yard").val(yard);
+
+		var inch = mile * 2.544e+14;
+		$(".inch").val(inch);
+	}
+	</script>
+
+	<script>
+	function from_ccf()
+	{
+		var ccf = $(".ccf").val();
+
+		var mile = ccf * 6.79357e-10;
+		$(".mile").val(mile);
+
+		var mcf = ccf * 0.1;
+		$(".mcf").val(mcf);
+
+		var foot = ccf * 100;
+		$(".foot").val(foot);
+
+		var yard = ccf * 3.7037;
+		$(".yard").val(yard);
+
+		var inch = ccf * 172800;
+		$(".inch").val(inch);
+	}
+	</script>
+
+	<script>
+	function from_mcf()
+	{
+		var mcf = $(".mcf").val();
+
+		var mile = mcf * 6.79357e-12;
+		$(".mile").val(mile);
+
+		var ccf = mcf * 10;
+		$(".ccf").val(ccf);
+
+		var foot = mcf * 1000;
+		$(".foot").val(foot);
+
+		var yard = mcf * 37.04;
+		$(".yard").val(yard);
+
+		var inch = mcf * 1728000;
+		$(".inch").val(inch);
+	}	
+	</script>
+
+	<script>
+	function from_foot()
+	{
+		var foot = $(".foot").val();
+
+		var mile = foot * 6.79357e-12;
+		$(".mile").val(mile);
+
+		var ccf = foot * 0.01;
+		$(".ccf").val(ccf);
+
+		var mcf = foot * 0.001;
+		$(".mcf").val(mcf);
+
+		var yard = foot * 0.037037;
+		$(".yard").val(yard);
+
+		var inch = foot * 1728;
+		$(".inch").val(inch);
+	}	
+	</script>
+
+	<script>
+	function from_yard()
+	{
+		var yard = $(".yard").val();
+
+		var mile = yard * 1.83426e-10;
+		$(".mile").val(mile);
+
+		var ccf = yard * 0.27;
+		$(".ccf").val(ccf);
+
+		var mcf = yard * 0.027;
+		$(".mcf").val(mcf);
+
+		var foot = yard * 27;
+		$(".foot").val(foot);
+
+		var inch = yard * 46656;
+		$(".inch").val(inch);
+	}	
+	</script>
+
+	<script>
+	function from_inch()
+	{
+		var inch = $(".inch").val();
+
+		var mile = inch * 3.93147e-15;
+		$(".mile").val(mile);
+
+		var ccf = inch * 5.78704e-6;
+		$(".ccf").val(ccf);
+
+		var mcf = inch * 0.0000005787;
+		$(".mcf").val(mcf);
+
+		var foot = inch * 0.000578704;
+		$(".foot").val(foot);
+
+		var yard = inch * 2.14335e-5;
+		$(".yard").val(yard);
+	}	
+	</script>
   </body>
 </html>
